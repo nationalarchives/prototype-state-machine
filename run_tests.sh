6c85b0df1067376d8bf167baaa46f123c7198892
Sam Palmer <github@sampalmer.dev>
2019-06-12 09:36:02 +0100

    add tests for the other images

---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+container-structure-test test --image 247222723249.dkr.ecr.eu-west-2.amazonaws.com/clamav:latest --config virus/config.yml
+container-structure-test test --image 247222723249.dkr.ecr.eu-west-2.amazonaws.com/checksum:latest --config checksum/config.yml
+container-structure-test test --image 247222723249.dkr.ecr.eu-west-2.amazonaws.com/siegfried:latest --config fileformat/config.yml
