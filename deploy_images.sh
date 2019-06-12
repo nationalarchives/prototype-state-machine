fb400a87e61fd5dbae47a3257dab6a7edb092168
Sam Palmer <github@sampalmer.dev>
2019-06-12 10:19:00 +0100

    add commands to deploy script

---
+++ deploy_images.sh
@@ -0,0 +1,3 @@
+docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/checksum
+docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/siegfried
+docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/clamav
