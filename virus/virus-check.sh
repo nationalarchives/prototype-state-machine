f65ca8613c3507d5803bb5a384da3baed9477c64
Sam Palmer <github@sampalmer.dev>
2019-08-06 16:34:45 +0100

    Small updates to script

--- virus/virus-check.sh
+++ virus/virus-check.sh
@@ -5,5 +5,6 @@ if [[ $STATUS != "OK" ]]; then
     VIRUS_FOUND=true
 fi
 JSON=`jq -n --arg filename "$FILE_NAME" --arg status "$STATUS" --arg virusFound "$VIRUS_FOUND" '{filename: $filename, status: $status, virusFound: $virusFound}'`
+echo "$JSON"
 aws stepfunctions send-task-success --task-token $TASK_TOKEN_ENV_VARIABLE --task-output "$JSON"
 
