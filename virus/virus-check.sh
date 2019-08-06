f7438571ef595788569150392413fb22b8141852
Sam Palmer <github@sampalmer.dev>
2019-08-06 15:41:30 +0100

    Update dockerfiles and add virus check script

---
+++ virus/virus-check.sh
@@ -0,0 +1,9 @@
+aws s3 cp s3://tdr-files/$FILE_NAME .
+STATUS=`clamscan | head -n 1 | awk -F': ' '{print $2}'`
+VIRUS_FOUND=false
+if [[ $STATUS != "OK" ]]; then
+    VIRUS_FOUND=true
+fi
+JSON=`jq -n --arg filename "$FILE_NAME" --arg status "$STATUS" --arg virusFound "$VIRUS_FOUND" '{filename: $filename, status: $status, virusFound: $virusFound}'`
+aws stepfunctions send-task-success --task-token $TASK_TOKEN_ENV_VARIABLE --task-output "$JSON"
+
