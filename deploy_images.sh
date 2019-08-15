9187c1aebbda4758739409a053fe58b48f7ca08c
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-08-15 13:47:59 +0100

    Deploy to Docker Hub

--- deploy_images.sh
+++ deploy_images.sh
@@ -1,3 +1,7 @@
-docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/checksum
-docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/siegfried
-docker push 247222723249.dkr.ecr.eu-west-2.amazonaws.com/clamav
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker push nationalarchives/tdr-checksum-check:$1
+
+docker push nationalarchives/tdr-virus-check:$1
+
+docker push nationalarchives/tdr-file-format-check:$1
