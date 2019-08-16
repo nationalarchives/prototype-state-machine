a026b41d94d63f73ea26747f1cb3322c4e38d94f
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-08-16 08:00:52 +0100

    Hard code dev tag to see if will deploy to docker hub

--- deploy_images.sh
+++ deploy_images.sh
@@ -1,7 +1,7 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker push nationalarchives/tdr-checksum-check
+docker push nationalarchives/tdr-checksum-check:$1
 
-docker push nationalarchives/tdr-virus-check
+docker push nationalarchives/tdr-virus-check:$1
 
-docker push nationalarchives/tdr-file-format-check
+docker push nationalarchives/tdr-file-format-check:$1
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
-container-structure-test test --image nationalarchives/tdr-virus-check:latest --config virus/config.yml
-container-structure-test test --image nationalarchives/tdr-checksum-check:latest --config checksum/config.yml
-container-structure-test test --image nationalarchives/tdr-file-format-check:latest --config fileformat/config.yml
+container-structure-test test --image nationalarchives/tdr-virus-check:dev --config virus/config.yml
+container-structure-test test --image nationalarchives/tdr-checksum-check:dev --config checksum/config.yml
+container-structure-test test --image nationalarchives/tdr-file-format-check:dev --config fileformat/config.yml
