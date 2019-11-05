7e8a60aa7d22ab53d701ac12a720255d7b91671e
Sam Palmer <github@sampalmer.dev>
2019-11-05 09:38:42 +0000

    Always responds with OK for empty files
    The commit earlier was ignoring them but this just responds with OK for
    all empty files. If they're ignored, then the processing will never
    complete.

--- virus/process_file.py
+++ virus/process_file.py
@@ -4,9 +4,11 @@ import subprocess
 
 with open('out.log') as reader:
     for line in reader.readlines():
-        if "/viruscheck" in line and "Empty file" not in line:
+        if "/viruscheck" in line:
             parts = line.split(":")
             file_path = str(os.environ['CONSIGNMENT_ID']) + parts[0][11:]
             result = {"status": parts[1].strip(), "filename": file_path}
+            if "Empty file" in line:
+                result = {"status": "OK", "filename": file_path}
             subprocess.call(["aws","sns","publish","--topic-arn","arn:aws:sns:eu-west-2:247222723249:virus-check-result-dev","--message",json.dumps(result)])
         
