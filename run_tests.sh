3d2b6ffecce39cc0ce99b73acabe0960599a67cf
Sam Palmer <github@sampalmer.dev>
2019-11-05 09:58:46 +0000

    Put the correct tag on the image
    This would need to be a parameter if we ever use this in production but
    for now, this is fine.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
-container-structure-test test --image  nationalarchives/tdr-virus-check --config virus/config.yml
-container-structure-test test --image nationalarchives/tdr-checksum-check  --config checksum/config.yml
-container-structure-test test --image nationalarchives/tdr-file-format-check --config fileformat/config.yml
+container-structure-test test --image nationalarchives/tdr-virus-check:dev --config virus/config.yml
+container-structure-test test --image nationalarchives/tdr-checksum-check:dev  --config checksum/config.yml
+container-structure-test test --image nationalarchives/tdr-file-format-check:dev --config fileformat/config.yml
